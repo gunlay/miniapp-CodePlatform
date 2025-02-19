0186f852c4a682b10192e5473aa96c9fda9a240b
gunlay <gunlaywin@gmail.com>
2025-02-19 10:54:45 +0800

    首页demo数据填充

--- miniprogram/pages/home/index.ts
+++ miniprogram/pages/home/index.ts
@@ -1,5 +1,35 @@
 Page({
-  data: {},
+  data: {
+    leftList: [
+      {
+        id: 1,
+        image: '/images/content/python1.png',
+        title: '每晚 8 点后可接Python',
+        price: '200',
+        orderCount: '5000',
+        userAvatar: '/images/avatars/avatar1.png',
+        userName: '程序员的微信昵称',
+        userTitle: '百度前端工程师',
+        isVerified: true
+      },
+      // 可以添加更多数据
+    ],
+    rightList: [
+      {
+        id: 2,
+        image: '/images/content/python2.png',
+        title: '每晚 8 点后可接Python',
+        price: '200',
+        orderCount: '5000',
+        userAvatar: '/images/avatars/avatar2.png',
+        userName: '程序员的微信昵称',
+        userTitle: '前端工程师',
+        isVerified: false
+      },
+      // 可以添加更多数据
+    ]
+  },
+
   onLoad() {
     // 检查角色
     const { checkRole } = require('../../utils/role')
